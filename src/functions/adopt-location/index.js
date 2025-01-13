55e8fe22308bcd6dcc8291e287363bf828ae9bbd
Justus Witmer <67343580+justuswitmer@users.noreply.github.com>
2025-01-13 11:42:13 -0600

    Add no-auth version to flow-sense-total

--- src/functions/adopt-location/index.js
+++ src/functions/adopt-location/index.js
@@ -6,7 +6,6 @@ const { app } = require("@azure/functions");
 
 app.http("adopt-location", {
   methods: ["POST"],
-  authLevel: "anonymous",
   handler: async (request, context) => {
     context.log(`Http function processed request for url "${request.url}"`);
 
--- src/functions/flow-sense-total/index.js
+++ src/functions/flow-sense-total/index.js
@@ -5,70 +5,44 @@ const Auth = require("../../../auth");
 const ErrorHandler = require("../../../errorHandler");
 const Common = require("../../../common");
 
-app.http("flow-sense-total", {
-  methods: ["GET"],
-  authLevel: "anonymous",
-  handler: async (req, context) => {
-    try {
-      req = Common.parseRequest(req);
-      // Authenticate and authorize the user
-      const authorizedUser = await Auth.authorizeUser(req, db, {
-        requireBusinessId: true, // Adjust based on your auth requirements
-      });
+const getFlowSenseData = async (req, context) => {
+  try {
+    let out = []; // For collecting the output later
 
-      // Validate input
-      const validator = new Validator(req.query, {
-        gatewayId: "string|required",
-        deviceId: "string", // Not required, as we may want to query all devices, even though for flow-sense there should only ever be one per location
-        bucketMinutes: "integer|required", // To aggregate data https://docs.timescale.com/use-timescale/latest/time-buckets/
-        start: "iso8601",
-        end: "iso8601",
-        dev: "boolean",
-      });
+    // Prepare parameters for SQL query
+    const params = [];
+    const predicates = [];
 
-      if (validator.fails()) {
-        throw validator.errors;
-      }
+    const defaultStart = new Date(
+      new Date(new Date().setDate(new Date().getDate() - 14)).setHours(
+        0,
+        0,
+        0,
+        0
+      )
+    ).toISOString();
 
-      // Ensure the user has access to the requested data
-      await Auth.canAccessDevice(req.query.gatewayId, authorizedUser, db);
+    // no if as this is required
+    predicates.push(`"gateway_id" = $${params.length + 1}`);
+    params.push(req.query.gatewayId);
 
-      let out = []; // For collecting the output later
+    if (req.query.deviceId) {
+      predicates.push(`"device_id" = $${params.length + 1}`);
+      params.push(req.query.deviceId);
+    }
+    if (req.query.start) {
+      predicates.push(`time >= $${params.length + 1}::timestamptz`);
+      params.push(req.query.start);
+    } else {
+      predicates.push(`time >= $${params.length + 1}::timestamptz`);
+      params.push(defaultStart); // default to 14 days ago
+    }
+    if (req.query.end) {
+      predicates.push(`time <= $${params.length + 1}::timestamptz`);
+      params.push(req.query.end);
+    }
 
-      // Prepare parameters for SQL query
-      const params = [];
-      const predicates = [];
-
-      const defaultStart = new Date(
-        new Date(new Date().setDate(new Date().getDate() - 14)).setHours(
-          0,
-          0,
-          0,
-          0
-        )
-      ).toISOString();
-
-      // no if as this is required
-      predicates.push(`"gateway_id" = $${params.length + 1}`);
-      params.push(req.query.gatewayId);
-
-      if (req.query.deviceId) {
-        predicates.push(`"device_id" = $${params.length + 1}`);
-        params.push(req.query.deviceId);
-      }
-      if (req.query.start) {
-        predicates.push(`time >= $${params.length + 1}::timestamptz`);
-        params.push(req.query.start);
-      } else {
-        predicates.push(`time >= $${params.length + 1}::timestamptz`);
-        params.push(defaultStart); // default to 14 days ago
-      }
-      if (req.query.end) {
-        predicates.push(`time <= $${params.length + 1}::timestamptz`);
-        params.push(req.query.end);
-      }
-
-      const query = `
+    const query = `
       SELECT
         time_str AS time, -- undoing the rename from the subquery
         device_id,
@@ -92,40 +66,40 @@ app.http("flow-sense-total", {
       ) result
     `;
 
-      // context.log(query);
-      let result = await db.tsdbQuery(query, params);
+    // context.log(query);
+    let result = await db.tsdbQuery(query, params);
 
-      if (result.rows.length === 0) {
-        // ... and the query returned no data...
-        context.log("No data found");
-        if (req.query.dev) {
-          // If we're in dev mode...
-          context.log(
-            "We're in dev mode, so we'll return some fake data instead."
-          );
-          // ...build some fake data
-          const fakeDataParams = [];
-          const fakeMacAddress = Common.generateRandomMac();
+    if (result.rows.length === 0) {
+      // ... and the query returned no data...
+      context.log("No data found");
+      if (req.query.dev) {
+        // If we're in dev mode...
+        context.log(
+          "We're in dev mode, so we'll return some fake data instead."
+        );
+        // ...build some fake data
+        const fakeDataParams = [];
+        const fakeMacAddress = Common.generateRandomMac();
 
-          // no if as this is required
-          fakeDataParams.push(fakeMacAddress);
+        // no if as this is required
+        fakeDataParams.push(fakeMacAddress);
 
-          if (req.query?.start) {
-            fakeDataParams.push(req.query.start);
-          } else {
-            fakeDataParams.push(defaultStart); // default to 14 days ago
-          }
-          if (req.query?.end) {
-            fakeDataParams.push(req.query.end);
-          } else {
-            const defaultEnd = new Date().toISOString();
-            fakeDataParams.push(defaultEnd); // default to now
-          }
+        if (req.query?.start) {
+          fakeDataParams.push(req.query.start);
+        } else {
+          fakeDataParams.push(defaultStart); // default to 14 days ago
+        }
+        if (req.query?.end) {
+          fakeDataParams.push(req.query.end);
+        } else {
+          const defaultEnd = new Date().toISOString();
+          fakeDataParams.push(defaultEnd); // default to now
+        }
 
-          context.log(fakeDataParams);
+        context.log(fakeDataParams);
 
-          result = await db.tsdbQuery(
-            `
+        result = await db.tsdbQuery(
+          `
           SELECT
             *,
             $1 AS device_id
@@ -166,21 +140,89 @@ app.http("flow-sense-total", {
           ) AS result
           ORDER BY time
         `,
-            fakeDataParams
-          );
-        }
+          fakeDataParams
+        );
+      }
+    }
+
+    // context.log(out);
+    out = result.rows;
+    context.log(out.length + " rows returned");
+
+    return {
+      body: JSON.stringify(out),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    // Handle errors
+    return {
+      body: JSON.stringify(ErrorHandler.prepareResponse(context, error)),
+    };
+  }
+};
+
+app.http("flow-sense-total-data", {
+  methods: ["GET"],
+  handler: async (req, context) => {
+    try {
+      req = Common.parseRequest(req);
+
+      // Validate input
+      const validator = new Validator(req.query, {
+        gatewayId: "string|required",
+        deviceId: "string", // Not required, as we may want to query all devices, even though for flow-sense there should only ever be one per location
+        bucketMinutes: "integer|required", // To aggregate data https://docs.timescale.com/use-timescale/latest/time-buckets/
+        start: "iso8601",
+        end: "iso8601",
+        dev: "boolean",
+      });
+
+      if (validator.fails()) {
+        throw validator.errors;
       }
 
-      // context.log(out);
-      out = result.rows;
-      context.log(out.length + " rows returned");
-
+      const flowSenseData = await getFlowSenseData(req, context);
+      return { ...flowSenseData };
+    } catch (error) {
+      // Handle errors
       return {
-        body: JSON.stringify(out),
-        headers: {
-          "Content-Type": "application/json",
-        },
+        body: JSON.stringify(ErrorHandler.prepareResponse(context, error)),
       };
+    }
+  },
+});
+
+app.http("flow-sense-total", {
+  methods: ["GET"],
+  handler: async (req, context) => {
+    try {
+      req = Common.parseRequest(req, context);
+      // Authenticate and authorize the user
+      const authorizedUser = await Auth.authorizeUser(req, db, {
+        requireBusinessId: true, // Adjust based on your auth requirements
+      });
+
+      // Validate input
+      const validator = new Validator(req.query, {
+        gatewayId: "string|required",
+        deviceId: "string", // Not required, as we may want to query all devices, even though for flow-sense there should only ever be one per location
+        bucketMinutes: "integer|required", // To aggregate data https://docs.timescale.com/use-timescale/latest/time-buckets/
+        start: "iso8601",
+        end: "iso8601",
+        dev: "boolean",
+      });
+
+      if (validator.fails()) {
+        throw validator.errors;
+      }
+
+      // Ensure the user has access to the requested data
+      await Auth.canAccessDevice(req.query.gatewayId, authorizedUser, db);
+
+      const flowSenseData = await getFlowSenseData(req, context);
+      return { ...flowSenseData };
     } catch (error) {
       // Handle errors
       return {
