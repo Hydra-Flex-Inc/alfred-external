6c863dbf3246a5f70b250aeee50530a2a802f27e
Justus Witmer <67343580+justuswitmer@users.noreply.github.com>
2025-01-15 10:38:20 -0600

    Add auth/no-auth version of location-name

--- src/functions/location-name/index.js
+++ src/functions/location-name/index.js
@@ -5,6 +5,46 @@ const ErrorHandler = require("../../../errorHandler");
 const Validator = require("../../../validator");
 const Common = require("../../../common");
 
+const getLocationNameData = async (req, context) => {
+  try {
+    let carWashName = "Chem-Sense Dashboard";
+
+    const query = `
+    SELECT
+      l.id,
+      l.display_name,
+      l.city,
+      l.region
+    FROM
+      locations l
+      LEFT JOIN gateways g
+        ON l.id = g.location_id
+    WHERE
+      g.iot_hub_device_id = '${req.query.gatewayId}'
+  `;
+    const result = await db.query(query);
+    const location = result?.rows[0] || {};
+    if (location.display_name) {
+      carWashName = location.display_name;
+      if (location.city && location.region) {
+        carWashName += ` @ ${location.city}, ${location.region}`;
+      }
+    } else {
+      carWashName += ` (${req.query.gatewayId})`;
+    }
+    return {
+      body: JSON.stringify(carWashName),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    return {
+      body: JSON.stringify(ErrorHandler.prepareResponse(context, error)),
+    };
+  }
+};
+
 app.http("location-name", {
   methods: ["GET"],
   handler: async (req, context) => {
@@ -23,31 +63,36 @@ app.http("location-name", {
         throw validator.errors;
       }
 
-      let carWashName = "Chem-Sense Dashboard";
+      const carWashName = await getLocationNameData(req, context);
+      return {
+        body: JSON.stringify(carWashName),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+    } catch (error) {
+      return {
+        body: JSON.stringify(ErrorHandler.prepareResponse(context, error)),
+      };
+    }
+  },
+});
 
-      const query = `
-    SELECT
-      l.id,
-      l.display_name,
-      l.city,
-      l.region
-    FROM
-      locations l
-      LEFT JOIN gateways g
-        ON l.id = g.location_id
-    WHERE
-      g.iot_hub_device_id = '${req.query.gatewayId}'
-  `;
-      const result = await db.query(query);
-      const location = result?.rows[0] || {};
-      if (location.display_name) {
-        carWashName = location.display_name;
-        if (location.city && location.region) {
-          carWashName += ` @ ${location.city}, ${location.region}`;
-        }
-      } else {
-        carWashName += ` (${req.query.gatewayId})`;
+app.http("location-name-data", {
+  methods: ["GET"],
+  handler: async (req, context) => {
+    try {
+      req = Common.parseRequest(req);
+
+      const validator = new Validator(req.query, {
+        gatewayId: "required|string",
+      });
+
+      if (validator.fails()) {
+        throw validator.errors;
       }
+
+      const carWashName = await getLocationNameData(req, context);
       return {
         body: JSON.stringify(carWashName),
         headers: {
